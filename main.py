87528edf5ca0d466d75a905e11d699fc399b5396
haamseongho <haams704@gmail.com>
2021-02-03 23:45:07 +0900

    BabyGin

--- main.py
+++ main.py
@@ -7,7 +7,4 @@ import babygin
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    babygin.Solution()
-
-
-    # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    babygin.Solution()  # Baby-gin 문제
