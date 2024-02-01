8ace19a7e36d29dc85c48d863599ace908154b13
함성호 <hamseongho@hamseonghoui-MacBookAir.local>
2024-02-02 00:08:35 +0900

    feat: 옹알이 완료

---
+++ Javascript/옹알이.js
@@ -0,0 +1,15 @@
+function solution(babbling) {
+    // 4가지로 만들 수 있는 옹알이의 갯수: 4C1 * 4C2 * 4C3 * 4C4
+    var answer = 0;// 테스트
+    const inputArray = ["aya", "ye", "woo", "ma"];
+    for(var i=0;i<babbling.length;i++){
+        for(var j=0;j<inputArray.length;j++){
+            babbling[i] = babbling[i].replace(inputArray[j], " ");
+        }
+        if(babbling[i].trim().length == 0){
+            answer += 1;
+        }
+    }
+    
+    return answer;
+}
